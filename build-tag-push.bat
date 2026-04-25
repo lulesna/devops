5b6ae605f5d830ad60ce73a1296f4ff5b08963bc
Lucja Lesna <lesna.lucja@gmail.com>
2026-04-25 11:46:52 +0200

    aktualizacja build-tag-push.bat

--- build-tag-push.bat
+++ build-tag-push.bat
@@ -1,5 +1,8 @@
+docker stop app nginx-proxy
+docker rm app nginx-proxy
+
 docker build -t product-dashboard-backend:v2 --build-arg IMAGE_VERSION=v2 ./backend/
-docker build -t product-dashboard-frontend:v2 --build-arg ./frontend/
+docker build -t product-dashboard-frontend:v2 --build-arg IMAGE_VERSION=v2 ./frontend/
 
 docker tag product-dashboard-backend:v2 llesna/product-dashboard-backend:v2
 docker tag product-dashboard-backend:v2 llesna/product-dashboard-backend:latest
