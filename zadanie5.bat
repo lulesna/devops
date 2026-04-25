a47b12f77275e48748c4d64281f0397769104556
Lucja Lesna <lesna.lucja@gmail.com>
2026-04-25 18:05:25 +0200

    skrypt do zadania 5

--- zadanie5.bat
+++ zadanie5.bat
@@ -19,6 +19,3 @@ docker network create back-net
 docker run -d --name app --network back-net -v items-data:/data llesna/product-dashboard-backend:v2
 docker run -d --name nginx-proxy --network front-net -p 80:80 llesna/product-dashboard-frontend:v2
 docker network connect back-net nginx-proxy
-
-curl.exe http://localhost/api/items
-curl.exe http://localhost/api/stats
